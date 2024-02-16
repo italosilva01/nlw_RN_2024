8c4ea7a265253cac49b4c36f700167d53fed3115
Francisco Italo <italo.silvan87@gmail.com>
2024-02-16 08:47:49 -0300

    add part of second class

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,29 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { FlatList, View } from "react-native";
-
-import { CATEGORIES } from "@/utils/data/products";
-import { useState } from "react";
+import { FlatList, View, SectionList, Text } from "react-native";
+import { Link } from "expo-router";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+import { useState, useRef } from "react";
+import { Product } from "@/components/product";
 export default function Home() {
   const [categorySelected, setCategory] = useState(CATEGORIES[0]);
 
+  const sectionListRef = useRef<SectionList>(null);
+
   function handleCategorySelect(selectedCategory: string) {
     setCategory(selectedCategory);
+
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory
+    );
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      });
+    }
   }
   return (
     <View className=" flex-1 pt-8">
@@ -27,6 +42,26 @@ export default function Home() {
         className="max-h-10 mt-5"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
         showsHorizontalScrollIndicator={false}
+      />
+
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   );
